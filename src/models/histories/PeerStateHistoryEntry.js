8de5f83e7eb4dda77bd736fe4c43dfc54c96632b
z.wieczorek <zbyszek.wieczorek@gmail.com>
2016-10-31 11:09:01 +0100

    typings

---
+++ src/models/histories/PeerStateHistoryEntry.js
@@ -0,0 +1,28 @@
+"use strict";
+const AsteriskHistoryEntry = require("../../internal/asteriskHistoryEntry");
+/**
+ * An entry in the channel state history of an AsteriskChannel
+ * Creates a new instance.
+ */
+class PeerStateHistoryEntry extends AsteriskHistoryEntry {
+    get state() {
+        return this.entry;
+    }
+    ;
+    get stateDesc() {
+        return this.entry.name;
+    }
+    toString() {
+        let sb;
+        sb = "ChannelStateHistoryEntry[";
+        sb += "date=" + this.date.toString() + ",";
+        sb += "state=" + this.state.toString() + "]";
+        return sb;
+    }
+    ;
+    toJSON() {
+        return this.stateDesc;
+    }
+}
+module.exports = PeerStateHistoryEntry;
+//# sourceMappingURL=PeerStateHistoryEntry.js.map
